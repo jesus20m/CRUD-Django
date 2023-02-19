7ae3aea4744ed3d8f5a0e6392e19911de041801f
JesusGonzalez <jesus.gonzalezmon20@gmail.com>
2023-02-19 14:44:45 -0500

    Hello world

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tasks import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.helloworld)
 ]
